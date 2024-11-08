4b1e8b8205bd0cdad0425acd23f8a6e8852751b6
limichange <limichange@hotmail.com>
2024-11-08 22:11:47 +0800

    update api

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from tweeterpy import TweeterPy
+
+app = FastAPI()
+twitter = TweeterPy()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# get user info by screen name
+@app.get("/user/screen_name/{twitter_user_screen_name}")
+async def get_user_info(twitter_user_screen_name):
+    
+    user_data = twitter.get_user_data(twitter_user_screen_name)
+
+    return {"data": user_data}
