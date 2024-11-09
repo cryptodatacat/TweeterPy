7cd048eab94097cc3a7386ac41aa2353557363a0
limichange <limichange@hotmail.com>
2024-11-09 09:39:23 +0800

    add session

--- app/main.py
+++ app/main.py
@@ -12,7 +12,12 @@ async def root():
 # get user info by screen name
 @app.get("/user/screen_name/{twitter_user_screen_name}")
 async def get_user_info(twitter_user_screen_name):
-    twitter.generate_session()
-    user_data = twitter.get_user_data(twitter_user_screen_name)
+
+    try:
+        user_data = twitter.get_user_data(twitter_user_screen_name)
+    except Exception as e:
+        logging.error(e)
+        twitter.generate_session()
+        user_data = twitter.get_user_data(twitter_user_screen_name)
 
     return user_data
