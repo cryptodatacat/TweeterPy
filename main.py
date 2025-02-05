90ae804b2e907f3a638f897364b874ae0082fd79
limichange <limichange@hotmail.com>
2025-02-05 13:58:03 +0800

    feat: add nickname

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from tweeterpy import TweeterPy
+from tweeterpy import TweeterPy, config
+from twikit import Client
+from twikit.guest import GuestClient
+
 import logging
+import random
+
+client = GuestClient('en-US')
+
+proxy_object = {
+        "ip": "173.0.9.209",
+        "port": "5792",
+        "user": "rgkbavci",
+        "password": "900m3tjexikm"
+    }
+proxy_url = "socks5://" + proxy_object["user"] + ":" +proxy_object['password'] + "@" + proxy_object["ip"] + ":" + proxy_object["port"]
+proxies_formatted = {
+    "socks5": proxy_url,
+    # "http": proxy_url
+}
+
+config.PROXY = proxies_formatted
 
 app = FastAPI()
-twitter = TweeterPy()
+clients = [
+    TweeterPy(),
+]
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    await client.activate()
+    user = await client.get_user_by_screen_name('elonmusk')
+
+    return user.name
 
 # get user info by screen name
 @app.get("/user/screen_name/{twitter_user_screen_name}")
 async def get_user_info(twitter_user_screen_name):
-
+    t = random.choice(clients)
+    
     try:
-        user_data = twitter.get_user_data(twitter_user_screen_name)
+        user_data = t.get_user_data(twitter_user_screen_name)
         return user_data
     except Exception as e:
         print("retry: " + twitter_user_screen_name)
 
         try: 
-            twitter.generate_session()
-            user_data = twitter.get_user_data(twitter_user_screen_name)
+            t.generate_session()
+            t2 = random.choice(clients)
+            # t.request_client.session.proxies = proxies_formatted
+            user_data = t2.get_user_data(twitter_user_screen_name)
             return user_data
         except Exception as e:
             print("error: " + twitter_user_screen_name)
