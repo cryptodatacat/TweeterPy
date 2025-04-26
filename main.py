fa31d3fe0440e8e8b0e2335d61cb273a18bc490d
limichange <limichange@hotmail.com>
2025-04-26 20:26:34 +0800

    update v2

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from tweeterpy import TweeterPy, config
-
-config.UPDATE_API = False
-twitter = TweeterPy()
+from twikit import Client
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "elonmusk"
+    return "Hello World"
 
 @app.get("/SearchTimeline")
-async def get_user_info(rawQuery, authToken):
-    twitter.generate_session(auth_token=authToken)
-    search_query = rawQuery
-    return twitter.search(
-        search_query = search_query,
-        search_filter = "Latest",
-        pagination = False
-    )
+async def get_user_info(rawQuery, authToken, ct0):
+    client = Client("en-US")
+    client.set_cookies({
+        "auth_token": authToken,
+        "ct0": ct0,
+    })
+
+    response, _ = await client.gql.search_timeline(
+        rawQuery, "Latest", 20, None)
+    
+    return response
