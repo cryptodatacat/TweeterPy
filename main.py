188a041d01dc92020088fb85c306824af4726705
limichange <limichange@hotmail.com>
2025-04-26 15:12:25 +0800

    update

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from tweeterpy import TweeterPy, config
-from twikit import Client
-from twikit.guest import GuestClient
 
-import logging
-import random
-
-client = GuestClient('en-US')
-
+twitter = TweeterPy()
 app = FastAPI()
-clients = [
-    TweeterPy(),
-]
+
+twitter.update_api(False)
+twitter.generate_session(auth_token="5816384c85fb2734fd87ecbbe302a08f9ef80bed")
 
 @app.get("/")
 async def root():
-    await client.activate()
-    user = await client.get_user_by_screen_name('elonmusk')
+    return twitter.search("elonmusk")
 
-    return user.name
-
-# get user info by screen name
-@app.get("/user/screen_name/{twitter_user_screen_name}")
-async def get_user_info(twitter_user_screen_name):
-    t = random.choice(clients)
-    
-    try:
-        user_data = t.get_user_data(twitter_user_screen_name)
-        return user_data
-    except Exception as e:
-        print("retry: " + twitter_user_screen_name)
-
-        try: 
-            t.generate_session()
-            t2 = random.choice(clients)
-            # t.request_client.session.proxies = proxies_formatted
-            user_data = t2.get_user_data(twitter_user_screen_name)
-            return user_data
-        except Exception as e:
-            print("error: " + twitter_user_screen_name)
-
-            logging.error(e)
-            return JSONResponse(
-                status_code=400,
-                content={"message": "can't find user"}
-            )
+@app.get("/SearchTimeline")
+async def get_user_info(rawQuery):
+    search_query = rawQuery
+    return twitter.search(search_query)
