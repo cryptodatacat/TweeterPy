668500268ccba033c2746b8cef3383cf081a0f8b
limichange <limichange@hotmail.com>
2025-04-26 18:58:57 +0800

    update

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from tweeterpy import TweeterPy, config
 
+config.UPDATE_API = False
 twitter = TweeterPy()
-app = FastAPI()
 
-twitter.update_api(False)
-twitter.generate_session(auth_token="5816384c85fb2734fd87ecbbe302a08f9ef80bed")
+app = FastAPI()
 
 @app.get("/")
 async def root():
-    return twitter.search("elonmusk")
+    return "elonmusk"
 
 @app.get("/SearchTimeline")
-async def get_user_info(rawQuery):
+async def get_user_info(rawQuery, authToken):
+    twitter.generate_session(auth_token=authToken)
     search_query = rawQuery
     return twitter.search(
         search_query = search_query,
